3f44c5adf1e4b15bcf891e72203693d5b56ced7d
faezeh ebrahimy <fezebr2000@gmail.com>
2025-10-18 21:55:11 +0330

    chore: add tailwind config

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,3 +1,9 @@
 <template>
-  <div>home page</div>
+  <div class="mt-5">
+    <p class="test pt-5 bg-red-500">home page</p>
+  </div>
 </template>
+
+<script lang="ts" setup></script>
+
+<style></style>
