607f164e29dd13435e8c93d2338bd17a61f88dec
faezeh ebrahimy <fezebr2000@gmail.com>
2025-10-19 14:31:25 +0330

    feat: add name to routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,10 @@
 import CountryDetailsPage from '@/pages/CountryDetailsPage.vue'
 import HomePage from '@/pages/HomePage.vue'
 import { createRouter, createWebHistory } from 'vue-router'
- 
+
 const routes = [
-  { path: '/', component: HomePage },
-  { path: '/country/:code', component: CountryDetailsPage, props: true },
+  { path: '/', name: 'home', component: HomePage },
+  { path: '/country/:code', name: 'country-details', component: CountryDetailsPage },
 ]
 
 export default createRouter({
