29bfe14ce71325931846ce82686385a673b876f6
faezeh ebrahimy <fezebr2000@gmail.com>
2025-10-19 12:58:26 +0330

    fix: fix header styles

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,11 +1,10 @@
 <template>
   <header
-    class="py-8 flex justify-between items-center shadow-md px-4 md:px-16 bg-white"
+    class="py-6 flex justify-between items-center shadow-md px-4 md:px-16 bg-white"
   >
-    <h1 class="text-lg font-extrabold">Where in the world?</h1>
+    <h1 class="text-sm font-extrabold">Where in the world?</h1>
     <button
       class="flex items-center gap-2 text-sm font-semibold cursor-pointer"
-      @click="toggleTheme"
     >
       <span v-if="theme === 'light'">🌙 Dark Mode</span>
       <span v-else>☀️ Light Mode</span>
