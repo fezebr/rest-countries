23e67344da87927ab826aee80b3f4d81710f0b78
faezeh ebrahimy <fezebr2000@gmail.com>
2025-10-19 14:03:51 +0330

    chore: handle alias with vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import path from "path";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [
-    vue(), tailwindcss(),
-  ],
-})
+  plugins: [vue(),tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),  
+    },
+  },
+});
