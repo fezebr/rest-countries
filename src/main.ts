0a0e40e1be1fdec5a5cc470b9ce0fb17ac13014f
faezeh ebrahimy <fezebr2000@gmail.com>
2025-10-20 01:15:08 +0330

    chore: add fontawesome

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
-const app = createApp(App)
+library.add(faSearch);
 
-app.use(router) 
+const app = createApp(App);
 
-app.mount('#app')
+app.component('font-awesome-icon', FontAwesomeIcon);
+app.use(router);
+
+app.mount('#app');
