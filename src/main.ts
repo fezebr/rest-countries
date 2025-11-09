175544d23105ecd1e4f45bbc993f36ab9818bd9d
faezeh ebrahimy <fezebr2000@gmail.com>
2025-11-09 20:15:48 +0330

    chore: install vue query

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,25 @@ import router from './router';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import { faSearch, faArrowLeft, faChevronDown, faMoon, faSun } from '@fortawesome/free-solid-svg-icons';
+import { VueQueryPlugin } from '@tanstack/vue-query'
 
 library.add(faSearch, faArrowLeft, faChevronDown, faMoon, faSun);
 
 const app = createApp(App);
-
+const vueQueryOptions = {
+    queryClientConfig: {
+      defaultOptions: {
+        queries: {
+          retry: 2,
+          refetchOnWindowFocus: false,
+        },
+      },
+    },
+  }
+  
+  
 app.component('font-awesome-icon', FontAwesomeIcon);
 app.use(router);
+app.use(VueQueryPlugin, vueQueryOptions)
 
 app.mount('#app');
